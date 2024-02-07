9e82014e13938da35dc5c24e74fdc2f21be08273
Spentine <114318049+Spentine@users.noreply.github.com>
2024-02-07 15:48:09 -0800

    fix

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 import { convert, data } from "./converter.js"
 
+var valueElement = document.getElementById("valueChosen");
+var fromElement = document.getElementById("fromUnit");
+var toElement = document.getElementById("toUnit");
+var result = document.getElementById("result");
+
 function unitMenu() {
   let r = "";
   Object.keys(data).forEach((k) => {
@@ -13,22 +18,17 @@ function plural(unit, value) {
   if (value == 1) {
     return unit;
   }
-  return data[unit].plural
+  return data[unit].plural;
 }
 
 function updateResult() {
-  console.log("omg");
   const val = document.getElementById("valueChosen").value;
   const calc = convert(val, fromElement.value, toElement.value);
+  console.log(val, calc);
   result.innerHTML = `${val} ${plural(fromElement.value, val)} = ${calc} ${plural(toElement.value, calc)}`;
 }
 
 unitMenu();
-
-var valueElement = document.getElementById("valueChosen");
-var fromElement = document.getElementById("fromUnit");
-var toElement = document.getElementById("toUnit");
-var result = document.getElementById("result");
 
 valueElement.addEventListener("input", (event) => updateResult());
 fromElement.addEventListener("change", (event) => updateResult());
