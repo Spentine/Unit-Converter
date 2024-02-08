3b2dd9f5556204758268ddb2053c18e7e1c6c38d
Spentine <114318049+Spentine@users.noreply.github.com>
2024-02-07 19:06:07 -0800

    make more units, add custom units, round

--- converter.js
+++ converter.js
@@ -4,21 +4,57 @@ STANDARD UNITS:
 length: meter
 */
 
-export const data = {
-  "kilometer": ["length", 1000, "kilometers"],
+const standardUnits = {
   "meter": ["length", 1, "meters"],
+  "kilometer": ["length", 1000, "kilometers"],
   "decimeter": ["length", 0.1, "decimeters"],
   "centimeter": ["length", 0.01, "centimeters"],
   "millimeter": ["length", 0.001, "millimeters"],
+  "micrometer": ["length", 1e-6, "micrometers"],
+  "nanometer": ["length", 1e-9, "nanometers"],
+  "picometer": ["length", 1e-12, "picometers"],
+  "femtometer": ["length", 1e-15, "femtometers"],
+  "attometer": ["length", 1e-18, "attometers"],
+  "zeptometer": ["length", 1e-21, "zeptometers"],
+  "yoctometer": ["length", 1e-24, "yoctometers"],
+  "rontometer": ["length", 1e-27, "rontometers"],
+  "quectometer": ["length", 1e-30, "quectometers"],
+  "decameter": ["length", 10, "decameters"],
+  "hectometer": ["length", 100, "hectometers"],
+  "megameter": ["length", 1e6, "megameters"],
+  "gigameter": ["length", 1e9, "gigameters"],
+  "terameter": ["length", 1e12, "terameters"],
+  "petameter": ["length", 1e15, "petameters"],
+  "exameter": ["length", 1e18, "exameters"],
+  "zettameter": ["length", 1e21, "zettameters"],
+  "yottameter": ["length", 1e24, "yottameters"],
+  "ronnameter": ["length", 1e27, "ronnameters"],
+  "quettameter": ["length", 1e30, "quettameters"],
+  
+  "light-second": ["length", 299792458, "light-seconds"],
+  "light-minute": ["length", 17987547480, "light-minutes"],
+  "light-hour": ["length", 1079252848800, "light-hours"],
+  "light-day": ["length", 25902068371200, "light-days"],
+  "light-year": ["length", 9454254955488000, "light-years"],
+  "astronomical unit": ["length", 149597870700, "astronomical units"],
+  "parsec": ["length", 30856775814913700, "parsecs"],
   
   "inch": ["length", 0.0254, "inches"], // 2.54 / 100
   "foot": ["length", 0.3048, "feet"], // inch * 12
-  "mile": ["length", 1609.344, "miles"], // feet * 5280
+  "yard": ["length", 0.9144, "yards"], // foot * 3
+  "mile": ["length", 1609.344, "miles"], // foot * 5280
+  
 };
 
-Object.keys(data).forEach((k) => {
-  data[k] = {"type": data[k][0], "factor": data[k][1], "plural": data[k][2]};
-});
+export var data = {};
+
+export function importUnits(u) {
+  Object.keys(u).forEach((k) => {
+    data[k] = {"type": u[k][0], "factor": u[k][1], "plural": u[k][2]};
+  });
+}
+
+importUnits(standardUnits);
 
 export function convert(value=1, fromUnit="meter", toUnit="meter") {
   const fUnit = data[fromUnit];
--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-import { convert, data } from "./converter.js"
+import { convert, data, importUnits } from "./converter.js"
 
 var valueElement = document.getElementById("valueChosen");
 var fromElement = document.getElementById("fromUnit");
 var toElement = document.getElementById("toUnit");
 var result = document.getElementById("result");
+var roundoffElement = document.getElementById("roundoff");
+var customUnitsElement = document.getElementById("customUnits");
+var roundingCutoff = 10;
+var currentURL = window.location.href;
+var customUnits = localStorage.getItem("customUnits");
+
+roundoffElement.value = roundingCutoff;
+
+if (customUnits !== null) {
+  customUnitsElement.value = customUnits;
+  try {
+    customUnits = JSON.parse(customUnits);
+    importUnits(customUnits);
+  } catch {
+    console.log("invalid JSON!");
+  }
+}
 
 function unitMenu() {
   let r = "";
@@ -21,10 +38,34 @@ function plural(unit, value) {
   return data[unit].plural;
 }
 
+function setRounding() {
+  const r = roundoffElement.value;
+  roundingCutoff = Number(r);
+  console.log(roundingCutoff);
+}
+
+function roundOff(value, rounding) {
+  if (value <= 1e-80) {
+    return 0;
+  }
+  if (rounding === 0) {
+    return value;
+  }
+  const s = rounding - Math.floor(Math.log10(value));
+  if (s < 0) {
+    return value;
+  }
+  return Number(value.toFixed(s));
+}
+
+function saveUnits() {
+  localStorage.setItem("customUnits", customUnitsElement.value);
+}
+
 function updateResult() {
-  const val = document.getElementById("valueChosen").value;
-  const calc = convert(val, fromElement.value, toElement.value);
-  console.log(val, calc);
+  const val = Number(document.getElementById("valueChosen").value);
+  const calc = roundOff(convert(val, fromElement.value, toElement.value), roundingCutoff);
+  
   result.innerHTML = `${val} ${plural(fromElement.value, val)} = ${calc} ${plural(toElement.value, calc)}`;
 }
 
@@ -33,3 +74,5 @@ unitMenu();
 valueElement.addEventListener("input", (event) => updateResult());
 fromElement.addEventListener("change", (event) => updateResult());
 toElement.addEventListener("change", (event) => updateResult());
+roundoffElement.addEventListener("change", (event) => setRounding());
+customUnitsElement.addEventListener("change", (event) => saveUnits());
